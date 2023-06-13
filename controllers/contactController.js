5f56cbcbab03db51091de39ae8dc1d30de925efc
UzomaAkpala <francisakpala@yahoo.com>
2023-06-13 18:48:37 +0100

    updates

---
+++ controllers/contactController.js
@@ -0,0 +1,67 @@
+const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
+//@desc Get all contacts
+//@route Get /api/contacts
+//@access public
+
+const getContacts = asyncHandler(async (req, res) => {
+  const contacts = await Contact.find();
+  res.status(200).json(contacts);
+});
+
+//@desc create new contacts
+//@route post /api/contacts
+//@access public
+
+const createContact = asyncHandler(async (req, res) => {
+  console.log("The request body is", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are mandetory !");
+  }
+
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(201).json(contact);
+});
+
+//@desc Get contacts
+//@route GET /api/contacts/:id
+//@access public
+
+const getContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not Found!");
+  }
+  res.status(200).json(contact);
+});
+
+//@desc Update contacts
+//@route PUT /api/contacts/:id
+//@access public
+
+const updateContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Update contacts for ${req.params.id}` });
+});
+
+//@desc Delete contacts
+//@route DELETE /api/contacts/:id
+//@access public
+
+const deleteContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Delete contacts for ${req.params.id}` });
+});
+
+module.exports = {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+};
